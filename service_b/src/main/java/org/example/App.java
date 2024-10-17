a2f2c62ca73c1ae113c50e76ec6322fab2248c2a
unknown <vigneshpuma2@gmail.com>
2024-10-17 09:54:29 +0530

    Testing

--- service_b/src/main/java/org/example/App.java
+++ service_b/src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(processString("Github Mono Repo"));
+        System.out.println(processString("Github Mono Repo Service B"));
     }
 
     public static String processString(String input) {
